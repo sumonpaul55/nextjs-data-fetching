bbfe72e63f67a67b99d57b3ece0d4c202218f358
sumon paul <sumpaul100@gmail.com>
2023-12-07 01:13:29 +0600

    creating multiple apis

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,10 +1,23 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-
+// get api
 export const GET = () => {
     return NextResponse.json({ message: "hello world" })
 }
+// Post Api
+export const POST = () => {
+    return NextRequest()
+}
+// patch api
 
+
+
+
+// put Api
+
+
+
+// delete pai
 
 
 
