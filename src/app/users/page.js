fe1fa597ca2594170cece9a76b539b896703affb
sumon paul <sumpaul100@gmail.com>
2023-12-07 01:00:45 +0600

    css module next js similar class test

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,9 +1,10 @@
 import Users from '@/components/Users/Users';
 import React from 'react';
-
+import styles from "./Users.module.css"
 const UsersPage = () => {
     return (
         <div>
+            <h1 className={styles.pagetitle}>All Users</h1>
             <Users></Users>
         </div>
     );
